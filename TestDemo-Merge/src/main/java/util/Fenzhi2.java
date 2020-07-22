eaef23d90ba0c1efa1c6da43d6170eb87729cdfc
li_lingling <406813373@qq.com>
2020-07-22 21:19:29 +0800

    init commit

--- TestDemo-Merge/src/main/java/util/Fenzhi2.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Fenzhi2 {
-public static void main(String[] args) {
-	System.out.println("fenzhi2");
-}
-}
--- TestDemo-Merge/src/main/java/util/MasterTest.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class MasterTest {
-public static void main(String[] args) {
-	System.out.println("master:fenzhi");
-}
-}
